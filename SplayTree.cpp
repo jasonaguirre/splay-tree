2284ab1a3d4681077c7650bba84866372b6c5e2d
Gavin-Doherty <78055372+Gavin-Doherty@users.noreply.github.com>
2021-12-03 21:21:52 -0500

    SplayTree Code

--- SplayTree.cpp
+++ SplayTree.cpp
@@ -1,363 +1,245 @@
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-// data structure that represents a node in the tree
-struct Node {
-	int data; // holds the key
-	Node *parent; // pointer to the parent
-	Node *left; // pointer to left child
-	Node *right; // pointer to right child
+struct _node {
+	string data;
+	struct _node* left;
+	struct _node* right;
 };
 
-typedef Node *NodePtr;
+typedef struct _node node;
 
-// class SplayTree implements the operations in Splay tree
 class SplayTree {
 private:
-	NodePtr root;
+	node* root;
 
-	void preOrderHelper(NodePtr node) {
-		if (node != nullptr) {
-			cout<<node->data<<" ";
-			preOrderHelper(node->left);
-			preOrderHelper(node->right);
-		}
-	}
-
-	void inOrderHelper(NodePtr node) {
-		if (node != nullptr) {
-			inOrderHelper(node->left);
-			cout<<node->data<<" ";
-			inOrderHelper(node->right);
-		}
-	}
-
-	void postOrderHelper(NodePtr node) {
-		if (node != nullptr) {
-			postOrderHelper(node->left);
-			postOrderHelper(node->right);
-			cout<<node->data<<" ";
-		}
-	}
-
-	NodePtr searchTreeHelper(NodePtr node, int key) {
-		if (node == nullptr || key == node->data) {
-			return node;
-		}
-
-		if (key < node->data) {
-			return searchTreeHelper(node->left, key);
-		}
-		return searchTreeHelper(node->right, key);
-	}
-
-	void deleteNodeHelper(NodePtr node, int key) {
-		NodePtr x = nullptr;
-		NodePtr t, s;
-		while (node != nullptr){
-			if (node->data == key) {
-				x = node;
-			}
-
-			if (node->data <= key) {
-				node = node->right;
-			} else {
-				node = node->left;
-			}
-		}
-
-		if (x == nullptr) {
-			cout<<"Couldn't find key in the tree"<<endl;
+	void preorderHelper(node* curr) {
+		if (curr == NULL) {
 			return;
 		}
-		split(x, s, t); // split the tree
-		if (s->left){ // remove x
-			s->left->parent = nullptr;
-		}
-		root = join(s->left, t);
-		delete(s);
-		s = nullptr;
+		cout << curr->data << endl;
+		preorderHelper(curr->left);
+		preorderHelper(curr->right);
 	}
 
-	void printHelper(NodePtr root, string indent, bool last) {
-		// print the tree structure on the screen
-	   	if (root != nullptr) {
-		   cout<<indent;
-		   if (last) {
-		      cout<<"└────";
-		      indent += "     ";
-		   } else {
-		      cout<<"├────";
-		      indent += "|    ";
-		   }
-
-		   cout<<root->data<<endl;
-
-		   printHelper(root->left, indent, false);
-		   printHelper(root->right, indent, true);
+	void inorderHelper(node* curr) {
+		if (curr == NULL) {
+			return;
 		}
+		inorderHelper(curr->left);
+		cout << curr->data << endl;
+		inorderHelper(curr->right);
 	}
 
-	// rotate left at node x
-	void leftRotate(NodePtr x) {
-		NodePtr y = x->right;
-		x->right = y->left;
-		if (y->left != nullptr) {
-			y->left->parent = x;
+	void postorderHelper(node* curr) {
+		if (curr == NULL) {
+			return;
 		}
-		y->parent = x->parent;
-		if (x->parent == nullptr) {
-			this->root = y;
-		} else if (x == x->parent->left) {
-			x->parent->left = y;
-		} else {
-			x->parent->right = y;
-		}
-		y->left = x;
-		x->parent = y;
+		postorderHelper(curr->left);
+		postorderHelper(curr->right);
+		cout << curr->data << endl;
 	}
 
-	// rotate right at node x
-	void rightRotate(NodePtr x) {
-		NodePtr y = x->left;
-		x->left = y->right;
-		if (y->right != nullptr) {
-			y->right->parent = x;
-		}
-		y->parent = x->parent;
-		if (x->parent == nullptr) {
-			this->root = y;
-		} else if (x == x->parent->right) {
-			x->parent->right = y;
-		} else {
-			x->parent->left = y;
-		}
-		y->right = x;
-		x->parent = y;
+	node* createNode(string key) {
+		node* newNode = new node;
+		newNode->data = key;
+		newNode->left = NULL;
+		newNode->right = NULL;
+		return newNode;
 	}
 
-	// splaying
-	void splay(NodePtr x) {
-		while (x->parent) {
-			if (!x->parent->parent) {
-				if (x == x->parent->left) {
-					// zig rotation
-					rightRotate(x->parent);
-				} else {
-					// zag rotation
-					leftRotate(x->parent);
+	node* rightRotate(node* curr) {
+		node* leftChild = curr->left;
+		curr->left = leftChild->right;
+		leftChild->right = curr;
+		return leftChild;
+	}
+
+	node* leftRotate(node* curr) {
+		node* rightChild = curr->right;
+		curr->right = rightChild->left;
+		rightChild->left = curr;
+		return rightChild;
+	}
+
+	node* splay(node* curr, string key) {
+		if (curr == NULL) {
+			return NULL;
+		}
+		if (curr->data == key) {
+			return curr;
+		}
+
+		if (curr->data > key) {
+			if (curr->left == NULL) {
+				return curr;
+			}
+			if (curr->left->data > key) {
+				curr->left->left = splay(curr->left->left, key);
+				curr = rightRotate(curr);
+			}
+			else if (curr->left->data < key) {
+				curr->left->right = splay(curr->left->right, key);
+				if (curr->left->right != NULL) {
+					curr->left = leftRotate(curr->left);
 				}
-			} else if (x == x->parent->left && x->parent == x->parent->parent->left) {
-				// zig-zig rotation
-				rightRotate(x->parent->parent);
-				rightRotate(x->parent);
-			} else if (x == x->parent->right && x->parent == x->parent->parent->right) {
-				// zag-zag rotation
-				leftRotate(x->parent->parent);
-				leftRotate(x->parent);
-			} else if (x == x->parent->right && x->parent == x->parent->parent->left) {
-				// zig-zag rotation
-				leftRotate(x->parent);
-				rightRotate(x->parent);
+			}
+			if (curr->left != NULL) {
+				return rightRotate(curr);
 			} else {
-				// zag-zig rotation
-				rightRotate(x->parent);
-				leftRotate(x->parent);
+				return curr;
+			}
+		} else {
+			if (curr->right == NULL) {
+				return curr;
+			}
+			if (curr->right->data > key) {
+				curr->right->left = splay(curr->right->left, key);
+				if (curr->right->left != NULL) {
+					curr->right = rightRotate(curr->right);
+				}
+			}
+			else if (curr->right->data < key) {
+				curr->right->right = splay(curr->right->right, key);
+				curr = leftRotate(curr);
+			}
+			if (curr->right != NULL) {
+				return leftRotate(curr);
+			} else {
+				return curr;
 			}
 		}
 	}
 
-	// joins two trees s and t
-	NodePtr join(NodePtr s, NodePtr t){
-		if (!s) {
-			return t;
+	node* insertHelper(node* curr, string key) {
+		curr = splay(curr, key);
+		if (curr->data == key) {
+			return curr;
 		}
 
-		if (!t) {
-			return s;
-		}
-		NodePtr x = maximum(s);
-		splay(x);
-		x->right = t;
-		t->parent = x;
-		return x;
-	}
+		node* newNode = createNode(key);
 
-	// splits the tree into s and t
-	void split(NodePtr &x, NodePtr &s, NodePtr &t) {
-		splay(x);
-		if (x->right) {
-			t = x->right;
-			t->parent = nullptr;
+		if (curr->data > key) {
+			newNode->right = curr;
+			newNode->left = curr->left;
+			curr->left = NULL;
 		} else {
-			t = nullptr;
+			newNode->left = curr;
+			newNode->right = curr->right;
+			curr->right = NULL;
 		}
-		s = x;
-		s->right = nullptr;
-		x = nullptr;
+
+		return newNode;
 	}
+
+	node* deleteNode(node* curr, string key) {
+		if (curr == NULL) {
+			return curr;
+		}
+		if (curr->data > key) {
+			curr->left = deleteNode(curr->left, key);
+		}
+		else if (curr->data < key) {
+			curr->right = deleteNode(curr->right, key);
+		}
+		else {
+			if (curr->left == NULL && curr->right == NULL) {
+				return NULL;
+			}
+			else if (curr->left == NULL) {
+				return curr->right;
+			}
+			else if (curr->right == NULL) {
+				return curr->left;
+			}
+			node* minNode = curr;
+			while(minNode && minNode->left != NULL) {
+				minNode = minNode->left;
+			}
+
+			curr->data = minNode->data;
+			curr->right = deleteNode(curr->right, minNode->data);
+		}
+		return curr;
+	}
+
 
 public:
 	SplayTree() {
-		root = nullptr;
+		root = NULL;
 	}
 
-	// Pre-Order traversal
-	// Node->Left Subtree->Right Subtree
+	void insert(string key) {
+		if (root == NULL) {
+			root = createNode(key);
+		}
+
+		root = insertHelper(root, key);
+	}
+
+	bool search(string key) {
+		root = splay(root, key);
+		if (root->data == key) {
+			return true;
+		}
+		return false;
+	}
+
 	void preorder() {
-		preOrderHelper(this->root);
+		preorderHelper(root);
 	}
 
-	// In-Order traversal
-	// Left Subtree -> Node -> Right Subtree
-	void inorder() {
-		inOrderHelper(this->root);
-	}
-
-	// Post-Order traversal
-	// Left Subtree -> Right Subtree -> Node
 	void postorder() {
-		postOrderHelper(this->root);
+		postorderHelper(root);
 	}
 
-	// search the tree for the key k
-	// and return the corresponding node
-	NodePtr searchTree(int k) {
-		NodePtr x = searchTreeHelper(this->root, k);
-		if (x) {
-			splay(x);
-		}
-		return x;
+	void inorder() {
+		inorderHelper(root);
 	}
 
-	// find the node with the minimum key
-	NodePtr minimum(NodePtr node) {
-		while (node->left != nullptr) {
-			node = node->left;
-		}
-		return node;
-	}
-
-	// find the node with the maximum key
-	NodePtr maximum(NodePtr node) {
-		while (node->right != nullptr) {
-			node = node->right;
-		}
-		return node;
-	}
-
-	// find the successor of a given node
-	NodePtr successor(NodePtr x) {
-		// if the right subtree is not null,
-		// the successor is the leftmost node in the
-		// right subtree
-		if (x->right != nullptr) {
-			return minimum(x->right);
-		}
-
-		// else it is the lowest ancestor of x whose
-		// left child is also an ancestor of x.
-		NodePtr y = x->parent;
-		while (y != nullptr && x == y->right) {
-			x = y;
-			y = y->parent;
-		}
-		return y;
-	}
-
-	// find the predecessor of a given node
-	NodePtr predecessor(NodePtr x) {
-		// if the left subtree is not null,
-		// the predecessor is the rightmost node in the
-		// left subtree
-		if (x->left != nullptr) {
-			return maximum(x->left);
-		}
-
-		NodePtr y = x->parent;
-		while (y != nullptr && x == y->left) {
-			x = y;
-			y = y->parent;
-		}
-
-		return y;
-	}
-
-	// insert the key to the tree in its appropriate position
-	void insert(int key) {
-		// normal BST insert
-		NodePtr node = new Node; //creating new node 
-		node->parent = nullptr; //setting the key nodes parent to NULL
-		node->left = nullptr; //setting key nodes left child to NULL
-		node->right = nullptr; //setting the key nodes right child to Null
-		node->data = key; //setting the data in the node to the value of the "key" parameter
-		NodePtr y = nullptr; 
-		NodePtr x = this->root;
-
-		while (x != nullptr) {
-			y = x;
-			if (node->data < x->data) {
-				x = x->left;
-			} else {
-				x = x->right;
-			}
-		}
-
-		// y is parent of x
-		node->parent = y;
-		if (y == nullptr) {
-			root = node;
-		} else if (node->data < y->data) {
-			y->left = node;
-		} else {
-			y->right = node;
-		}
-
-		// splay the node
-		splay(node);
-	}
-
-	NodePtr getRoot(){
-		return this->root;
-	}
-
-	// delete the node from the tree
-	void deleteNode(int data) {
-		deleteNodeHelper(this->root, data);
-	}
-
-	// print the tree structure on the screen
-	void prettyPrint() {
-		printHelper(this->root, "", true);
+	void remove(string key) {
+		deleteNode(root, key);
 	}
 
 };
 
-int main() {
-	SplayTree bst;
-	bst.insert(33);
-	bst.insert(44);
-	bst.insert(67);
-	bst.insert(5);
-	bst.insert(89);
-	bst.insert(41);
-	bst.insert(98);
-	bst.insert(1);
-	bst.prettyPrint();
-	bst.searchTree(33);
-	bst.searchTree(44);
-	//bst.prettyPrint();
-	bst.deleteNode(89);
-	bst.deleteNode(67);
-	bst.deleteNode(41);
-	bst.deleteNode(5);
-	//bst.prettyPrint();
-	bst.deleteNode(98);
-	bst.deleteNode(1);
-	bst.deleteNode(44);
-	bst.deleteNode(33);
-	//bst.prettyPrint();
+int main(int argc, char const *argv[]){
+	SplayTree sTree;
+	ifstream inp(argv[1]);
+	if (inp.is_open()) {
+		string data;
+		while (inp >> data) {
+			sTree.insert(data);
+		}
+
+		cout << "Printing the tree in inorder: " << endl;
+		sTree.inorder();
+
+		cout << "\nPrinting the tree in preorder: " << endl;
+		sTree.preorder();
+
+		cout << "\nPrinting the tree in postorder: " << endl;
+		sTree.postorder();
+
+		cout << "\nEnter a node to search: ";
+		cin >> data;
+		if (sTree.search(data)) {
+			cout << data << " is found in the tree.\n\n";
+		} else {
+			cout << data << " is not found in the tree.\n\n";
+		}
+
+		cout << "\nEnter a node to delete: ";
+		cin >> data;
+		if (!sTree.search(data)) {
+			cout << data << " is not found in the tree.\n\n";
+		} else {
+			sTree.remove(data);
+			cout << "Node is deleted from the tree.\n";
+		}
+	}
 	return 0;
 }
